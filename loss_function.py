f3af5897769eb0d78660464e4e09394383f5c252
davidcombei <102519601+davidcombei@users.noreply.github.com>
2025-04-30 09:23:07 +0300

    Add files via upload

--- loss_function.py
+++ loss_function.py
@@ -20,7 +20,7 @@ torch_scaler = TorchScaler().to(device)
 
 
 class LMACLoss():
-    def __init__(self, l_in_w=4, l_out_w=0.4, reg_w_l1=0.4, reg_w_tv=0.01):
+    def __init__(self, l_in_w=4, l_out_w=0.15, reg_w_l1=0.1, reg_w_tv=0.00):
 
         self.l_in_w = l_in_w #default =4.0
         self.l_out_w = l_out_w #default 0.2
@@ -28,7 +28,7 @@ class LMACLoss():
         self.reg_w_tv = reg_w_tv #default 0.00
 
 
-    def loss_function(self, xhat, X_stft_mag ,X_stft_phase, class_pred,):
+    def loss_function(self, xhat, X_stft_mag ,X_stft_phase, yhat):
 
 
         Tmax = xhat.shape[1]
@@ -43,29 +43,34 @@ class LMACLoss():
         irrelevant_mask_mag = torch.expm1(irrelevant_log_mag)
 
         relevant_mask = relevant_mask_mag * torch.exp(1j * X_stft_phase[:, :Tmax, :])
-        irelevant_mask = irrelevant_mask_mag * torch.exp(1j * X_stft_phase[:, :Tmax, :])
+        irrelevant_mask = irrelevant_mask_mag * torch.exp(1j * X_stft_phase[:, :Tmax, :])
         
         
         istft_relevant_mask = audio_processor.compute_invert_stft(relevant_mask)
-        istft_irelevant_mask = audio_processor.compute_invert_stft(irelevant_mask)
+        istft_irrelevant_mask = audio_processor.compute_invert_stft(irrelevant_mask)
         
         
         relevant_mask_waveform = audio_processor.extract_features(istft_relevant_mask)
-        irelevant_mask_waveform = audio_processor.extract_features(istft_irelevant_mask)
+        irrelevant_mask_waveform = audio_processor.extract_features(istft_irrelevant_mask)
         relevant_mask_feats = torch.mean(relevant_mask_waveform.squeeze(0), dim=1)
-        irelevant_mask_feats = torch.mean(irelevant_mask_waveform.squeeze(0), dim=1)
+        irrelevant_mask_feats = torch.mean(irrelevant_mask_waveform.squeeze(0), dim=1)
         relevant_mask_logits, relevant_mask_probs = torch_logreg(relevant_mask_feats)
-        irelevant_mask_logits, irelevant_mask_probs = torch_logreg(irelevant_mask_feats)
-        
-        relevant_mask_logits = torch_scaler(relevant_mask_logits)
-        irelevant_mask_logits = torch_scaler(irelevant_mask_logits)
+        irrelevant_mask_logits, irrelevant_mask_probs = torch_logreg(irrelevant_mask_feats)
+#        relevant_mask_logits = torch_scaler(relevant_mask_logits)
+#        irrelevant_mask_logits = torch_scaler(irrelevant_mask_logits)
+#        relevant_mask_logits = torch_scaler(relevant_mask_logits)
+#        irrelevant_mask_logits = torch_scaler(irrelevant_mask_logits)
 
 
-        
-        l_in = F.binary_cross_entropy_with_logits(relevant_mask_logits, class_pred)#.to(device))
-        l_out = -F.binary_cross_entropy_with_logits(irelevant_mask_logits, class_pred)#.to(device))
+#        print(relevant_mask_logits)
+#        print(yhat)
+        l_in = F.binary_cross_entropy_with_logits(relevant_mask_logits, yhat)#.to(device))
+        l_out = -F.binary_cross_entropy_with_logits(irrelevant_mask_logits, yhat)#.to(device))
         ao_loss = self.l_in_w * l_in + self.l_out_w * l_out
-
+#        print(class_pred.shape)
+#        print(relevant_mask_logits.shape)
+#        print(l_in.shape)
+#        print(l_out.shape)
 
 #        reg_l1 = xhat.abs().mean((-1, -2)) * self.reg_w_l1
         reg_l1 = xhat.abs().mean((-1, -2, -3)) * self.reg_w_l1
@@ -74,6 +79,7 @@ class LMACLoss():
         reg_tv = self.reg_w_tv * (tv_h + tv_w) / float(power* xhat.size(0)) 
         reg_tv = self.reg_w_tv * tv_h / float(power * xhat.size(0))
         reg_loss = reg_l1 + reg_tv
-
+        
         total_loss = ao_loss + reg_loss
-        return total_loss
+ #       print(total_loss.shape)
+        return total_loss, l_in, l_out, reg_l1, relevant_mask_logits, irrelevant_mask_logits
--- train_addvisor.py
+++ train_addvisor.py
@@ -42,7 +42,7 @@ torch_scaler = TorchScaler().to(device)
 
 
 
-def find_all_wav_files(root_dir):
+def find_all_wav_files1(root_dir):
     audio_files = []
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for file in filenames:
@@ -53,15 +53,30 @@ def find_all_wav_files(root_dir):
 
 
 
+def find_all_wav_files2(root_dir, max_files=None):
+    audio_files = []
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for file in filenames:
+            if file.endswith(".wav"):
+                full_path = os.path.join(dirpath, file)
+                audio_files.append(full_path)
+                if max_files is not None and len(audio_files) >= max_files:
+                    return audio_files
+    return audio_files
+
+
 class AudioDataset(Dataset):
-    def __init__(self, directory, audio_processor, device):
-        self.file_paths = find_all_wav_files(directory)
+    def __init__(self, directory1, directory2, audio_processor, device):
+        files1 = find_all_wav_files2(directory1, max_files=10000)
+        files2 = find_all_wav_files2(directory2, max_files=10000)
+        self.file_paths = files1 + files2
         self.audio_processor = audio_processor
         self.device = device
 
+        
     def __len__(self):
         return len(self.file_paths)
-
+#        return 15000
     def __getitem__(self, idx):
         audio_path = self.file_paths[idx]
         waveform, sr = self.audio_processor.load_audio(audio_path)
@@ -74,55 +89,63 @@ def collate_fn(batch):
     features = audio_processor.extract_features(waveforms)
     feats_mean = torch.mean(features, dim=1)
     yhat_logits, _ = torch_log_reg(feats_mean)
-    yhat_logits = torch_scaler(yhat_logits)
-    thresh_tensor = torch.tensor(thresh, device=yhat_logits.device, dtype=yhat_logits.dtype)
-    class_pred = (yhat_logits > thresh_tensor).float()
-
-    return features, magnitude, phase, class_pred #, yhat_logits
-
+    yhat = torch_scaler(yhat_logits)
+    #thresh_tensor = torch.tensor(thresh, device=yhat_logits.device, dtype=yhat_logits.dtype)
+#    class_pred = (yhat_logits > thresh_tensor).float()
+    
+    return features, magnitude, phase, yhat_logits
 
 def train_addvisor(model, num_epochs, loss_fn, data_loader, save_path):
     for epoch in range(num_epochs):
         total_loss = 0
+        total_l_in,total_l_out,total_l1 = 0.0,0.0,0.0
+        total_nr_samples = len(data_loader)
+#        print(total_nr_samples)
         progress_bar = tqdm(data_loader, desc=f"training ADDvisor... epoch {epoch + 1}/{num_epochs}", dynamic_ncols=True, ascii=True)
         for i,batch in enumerate(progress_bar):
-                features, magnitude, phase, class_pred = batch
+                features, magnitude, phase, yhat_logits = batch
                 features = features.to(device)
                 magnitude = magnitude.to(device)
                 phase = phase.to(device)
-                class_pred = class_pred.to(device)
+ #               yhat = yhat.to(device)
                 mask = model(features)
-                loss_value = loss_fn.loss_function(mask, magnitude,phase, class_pred)
+                loss_value,  l_in, l_out, reg_l1, yhat_relevant, yhat_irrelevant = loss_fn.loss_function(mask, magnitude,phase, torch.sigmoid(yhat_logits))
                 optimizer.zero_grad()
                 loss_value.backward()
                 optimizer.step()
+#                print(f"loss_value: {loss_value}, shape: {loss_value.shape}")
+                total_l_in += l_in.item()
+                #print(total_l_in)
+                total_l_out += l_out.item()
+                total_l1 += reg_l1.item()
                 total_loss += loss_value.item()
+                if i == len(data_loader) - 1:
+                    last_yhat = yhat_logits
+                    last_yhat_relevant = yhat_relevant
+                    last_yhat_irrelevant = yhat_irrelevant
+                
                 progress_bar.set_postfix({'loss': f'{loss_value.item():.4f}'})
         avg_loss = total_loss / len(data_loader)
-        checkpoint_path = os.path.join(save_path, f"addvisor_MLAAD_epoch_{epoch+1}_loss_{avg_loss:.4f}.pth")
-    
+        checkpoint_path = os.path.join(save_path, f"addvisor_epoch_{epoch+1}_loss_{avg_loss:.4f}.pth")
+        log_line = f"Epoch {epoch+1}: l_in={total_l_in/total_nr_samples:.4f}, l_out={total_l_out/total_nr_samples:.4f}, L1={total_l1/total_nr_samples:.4f}, Logits_yhat_original:{last_yhat}, Logits_yhat_relevant: {last_yhat_relevant}, Logits_yhat_irrelevant: {last_yhat_irrelevant}\n"
+        with open("/mnt/QNAP/comdav/logs/ADDvisor_loss_terms.txt", "a") as f:
+            f.write(log_line)
         accelerator.save(model.state_dict(), checkpoint_path)
                          
 
 #dir_path = '/mnt/QNAP/comdav/partial_spoof/eval/con_wav/'
-dir_path = '/mnt/QNAP/comdav/MLAAD_v5/'
-save_path = '/mnt/QNAP/comdav/addvisor_savedV2/'
-
+dir_path1 = '/mnt/QNAP/comdav/MLAAD_v5/'
+dir_path2 = '/mnt/QNAP/comdav/m-ailabs/'
+save_path = '/mnt/QNAP/comdav/addvisor_savedV6/'
 
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-5)
 
 
-BATCH_SIZE = 24
-dataset = AudioDataset(directory = dir_path,
+BATCH_SIZE = 20
+dataset = AudioDataset(directory1 = dir_path1,
+                       directory2 = dir_path2,
                        audio_processor = audio_processor,
                        device = device)
-
 data_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
-
-
 model, optimizer, data_loader = accelerator.prepare(model, optimizer, data_loader)#, scheduler)
 train_addvisor(model=model, num_epochs=100, loss_fn=loss, data_loader=data_loader, save_path=save_path)
-
-
-
-
